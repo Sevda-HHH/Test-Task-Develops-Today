b1b4c72bdf95f45dc345496253179f2afdec880c
Sevda-HHH <SevdaMh@code.edu.az>
2021-08-01 10:31:07 +0400

    HttpClient added

---
+++ app/src/HttpClient/index.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export class HttpClient {
+    baseUrl;
+
+    constructor(url: string) {
+        this.baseUrl = url;
+    }
+
+    async get(url: string) {
+        return await axios.get(`${this.baseUrl}/${url}`)
+    }
+    async post(url: string, body: any) {
+        return await axios.post(`${this.baseUrl}/${url}`, body)
+    }
+}
